45ef1fddbf4a116c31ecd87c1772cdef26b814b4
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2026-02-18 10:26:14 +0100

    Deleting assets complete

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from Config import Config
 from ApiService import ApiService
 from TOPdesk import TOPdesk
 from Settings import Settings
+from IntuneDevice import IntuneDevice
 
-# TODO: we need update the Intune devices that need updating
-# TODO: we need a stable format for dates, as TOPdesk and intune have different formats
 # TODO: we need to link their users as well
 # TODO: we need to fetch the Lenovo data
 # TODO: we need to fetch the Microsoft Defender data
@@ -19,9 +18,13 @@ ApiService.get_azure_access_token()
 __intune_url = f"https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?$top={Settings.INTUNE_DEVICES_PER_FETCHED_PAGE}"
 next_page = __intune_url
 
+topdesk_assets_that_must_not_be_deleted = []
 while next_page:
     # fetch a page of intune devices, and the url for the next page
     current_page_devices, next_page = ApiService.get_devices_from_page(next_page)
+
+    for device in current_page_devices:
+        topdesk_assets_that_must_not_be_deleted.append(IntuneDevice(device).topdesk_asset_id)
 
     # create new assets if required
     TOPdesk.create_topdesk_assets(current_page_devices)
@@ -34,4 +37,4 @@ while next_page:
         next_page = None
 
 # keep assets that are not in Intune and delete them
-# TOPdesk.remove_device_assets_except(created_or_updated_devices)
+TOPdesk.remove_device_assets_except(topdesk_assets_that_must_not_be_deleted)
